b23b116824bec53fcb7de18fe8815d07bd3f48f7
Uzair <uzair@dynamiconlinetechnologies.com>
2021-06-25 00:40:33 +0500

    ...

--- app.js
+++ app.js
@@ -20,6 +20,23 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 app.use(cors());
+app.use((req, res, next) => {
+  const allowedOrigins = [
+    "http://127.0.0.1:8020",
+    "http://localhost:8020",
+    "http://127.0.0.1:9000",
+    "http://localhost:9000",
+  ];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
+  res.header("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", true);
+  return next();
+});
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

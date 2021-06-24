075de6168cb5bb47c71051b67107f655e4b5e791
Uzair <uzair@dynamiconlinetechnologies.com>
2021-06-25 00:28:35 +0500

    ...

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use("/", indexRouter);
 
 app.use("/nature", apiNatureRouter);
 app.use("/country", apiCountryRouter);
-
+Access - Control - Allow - Origin;
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
